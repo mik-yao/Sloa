7edf0abd416e56584608503e1a2c9526c044f521
mik-yao <tjrwns3179@gmail.com>
2025-08-22 15:34:45 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+$(function(){
+	$(".qna_box> ul> li").click(function(){
+		$(this).children(".qna_info").slideToggle();
+		$(this).siblings().children(".qna_info").slideUp();
+	});
+});
+$(function(){
+	$(".qna_box> ul> li").click(function(){
+		$(this).toggleClass("turn");
+		
+		if($(this).hasClass("turn")===true){
+				$(this).siblings().removeClass("turn");
+			
+		}
+
+	});
+});
+
+
+
+gsap.registerPlugin(SplitText);
+
+console.clear();
+
+document.fonts.ready.then(() => {
+  gsap.set(".split", { opacity: 1 });
+
+  let split;
+  SplitText.create(".split", {
+    type: "words,lines",
+    linesClass: "line",
+    autoSplit: true,
+    mask: "lines",
+    onSplit: (self) => {
+      split = gsap.from(self.lines, {
+        duration: 0.6,
+        yPercent: 100,
+        opacity: 0,
+        stagger: 0.1,
+        ease: "expo.out",
+      });
+      return split;
+    }
+  });
+
+  document.querySelector("button").addEventListener("click", (e) => {
+    split.timeScale(0.2).play(0);
+  });
+});
